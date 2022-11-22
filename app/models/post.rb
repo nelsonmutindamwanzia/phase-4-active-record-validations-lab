8ff3764fb443da1b5113b8c411f7d08699e161d2
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-11-22 16:01:11 +0300

    Complete Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    
+    #using inclusion
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+
+    CLICKBAIT_TITLES = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    
+    def clickbait?
+        if CLICKBAIT_TITLES.none? { |t| t.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
